57c8f9ca99ad8eff5912987e020d5acf8f3b6a89
GeonDong <ehd563@naver.com>
2023-11-20 15:57:04 +0900

    Feat: 라우터 관리  - 홈, 로그인, 회원가입, 게시물 라우터 개발

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 //  eslint-disable
-import React, { useEffect } from "react";
+import React from "react";
 import "./index.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./sass/main.scss";
+
+// components
+import Navbar from "./components/Navbar/Navbar";
 
 // pages
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login/Login";
 import Register from "./pages/Auth/Register/Register";
+import Ouath from "./pages/Auth/Oauth2/Ouath2";
 import Post from "./pages/Post/Post";
 import AddPost from "./pages/EditPost/AddPost/AddPost";
 import EditPost from "./pages/EditPost/EditPost/EditPost";
+import Search from "./pages/Search/SearchPage";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/oauth/kakao/callback" element={<Ouath />} />
         <Route path="/post/:id" element={<Post />} />
-        <Route path="/post" element={<AddPost />} />
         <Route path="/add" element={<AddPost />} />
         <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
       {/* <Footer /> */}
     </BrowserRouter>
