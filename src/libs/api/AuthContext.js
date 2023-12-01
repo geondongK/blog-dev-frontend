baeb3b97dea4579d9f28258d8544c020d46a4e33
GeonDong <ehd563@naver.com>
2023-12-01 17:13:24 +0900

    Modify: 토큰인증 axios 인터셉터  - 서버 토큰쿠키 인증 방식으로 수정

--- src/libs/api/AuthContext.js
+++ src/libs/api/AuthContext.js
@@ -1,95 +1,79 @@
-import axios from "axios";
+import instance from "./instance";
+import { useEffect } from "react";
 import { persistor } from "../../redux/store/store";
-import { getCookies, setCookies, removeCookies } from "../../utils/Cookies";
+import { useNavigate } from "react-router-dom";
 
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  timeout: 1000,
-  headers: {
-    "content-type": "application/json;charset=UTF-8",
-    accept: "application/json,",
-  },
-  withCredentials: true,
-});
+const logout = async () => {
+  await instance
+    .post("/auth/logout")
+    .then(() => {})
+    .catch(() => {});
+};
 
 const purge = async () => {
   await persistor.purge();
 };
 
-instance.interceptors.request.use(
-  (request) => {
-    const token = getCookies("token");
+// 헤더 인증 방식.
+// instance.interceptors.request.use(
+//   (request) => {
+//      console.log("requestreact", request);
+//     const token = getCookies("token");
 
-    request.headers["Authorization"] = `Bearer ${token}`;
+//     request.headers["Authorization"] = `Bearer ${token}`;
 
-    return request;
-  },
-  (error) => {
-    // console.log(error);
-    return Promise.reject(error);
-  }
-);
+//     return request;
+//   },
+//   (error) => {
+//      console.log(error);
+//     return Promise.reject(error);
+//   }
+// );
 
-instance.interceptors.response.use(
-  (response) => {
-    //console.log("response", response.data);
+export default function AuthContext() {
+  const navigate = useNavigate();
 
-    return response;
-  },
-  async (error) => {
-    const token = getCookies("token");
+  useEffect(() => {
+    const interceptor = instance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (error) => {
+        const {
+          config,
+          response: { status },
+        } = error;
 
-    const {
-      config,
-      response: { status },
-    } = error;
+        const originRequest = config;
 
-    const originRequest = config;
+        if (status === 401) {
+          if (error.response.data.message === "Unauthorized") {
+            // Refresh token API
+            const response = await instance.post("/auth/reissue");
 
-    if (status === 401) {
-      if (error.response.data.message === "Unauthorized") {
-        // Refresh token API
-        const response = await instance.post("/auth/reissue", {
-          accessToken: token,
-        });
+            if (response.status === 200) {
+              const newAccessToken = response.data.accessToken;
 
-        if (response.status === 200) {
-          const newAccessToken = response.data.accessToken;
-          const expiresTime = response.data.accessTokenExpiresIn;
+              originRequest.headers[
+                "Authorization"
+              ] = `Bearer ${newAccessToken}`;
 
-          const now = new Date().getTime();
-          // 현재 시간 + 만료시간
-          const expires = new Date(now + expiresTime * 1000);
-          // 로컬 스토리지에 현재 이용자 저장.
-
-          setCookies("token", newAccessToken, {
-            path: "/",
-            expires: expires,
-            httpOnly: process.env.REACT_APP_COOKIE_HTTPONLY,
-            secure: process.env.REACT_APP_COOKIE_SECURE,
-          });
-
-          originRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
-
-          return instance(originRequest);
-          // } else if (response.status === 404) {
-          //   window.alert("1");
-          // } else {
-          //   window.alert("2");
-          // }
+              return instance(originRequest);
+            }
+          }
         }
+        window.alert("세션이 만료되었습니다. 계속하려면 다시 로그인 하세요");
+        navigate("/");
+        purge();
+        logout();
+
+        return Promise.reject(error);
       }
-    }
+    );
 
-    window.alert("세션이 만료되었습니다. 계속하려면 다시 로그인 하세요");
-    purge();
-    removeCookies("token", {
-      maxAge: 0,
-      path: "/",
-    });
-    // navigate("/");
-    return Promise.reject(error);
-  }
-);
-
-export default instance;
+    return () => {
+      instance.interceptors.response.eject(interceptor);
+    };
+  }, []);
+  return <></>;
+}
