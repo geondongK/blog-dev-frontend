b80e69892bb3dc6c49f951823be07309666e1edf
GeonDong <ehd563@naver.com>
2023-12-01 17:10:59 +0900

    Add: AuthService 로그아웃  - 로그아웃 API 추가

--- src/libs/service/authService.js
+++ src/libs/service/authService.js
@@ -5,6 +5,11 @@ export const loginApi = (values) => {
   return customAxios.post("/auth/login", values);
 };
 
+/* 로그아웃 */
+export const logoutApi = () => {
+  return customAxios.post("auth/logout");
+};
+
 /* 회원가입 */
 export const registerApi = (values) => {
   return customAxios.post("/auth/signup", values);
